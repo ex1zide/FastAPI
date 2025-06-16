2733a0e1662f3143bbf12e80891bade6921e7e56
LINESKL <priveth06@gmail.com>
2025-06-16 18:08:36 +0500

    2 task

---
+++ 1_Биманов_Алишер/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import Field, SQLModel
+from typing import Optional
+from pydantic import BaseModel
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True, nullable=False)
+    password: str = Field(nullable=False)
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
